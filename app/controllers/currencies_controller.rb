d155ad06a5c0c94de45d678c9bdbe2df8af59218
jacobfain <jfain@chicagobooth.edu>
2021-04-28 03:31:34 +0000

    Final commit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -20,4 +20,22 @@ class CurrenciesController < ApplicationController
     render({:template => "currency_templates/step_two.html.erb"})
   end
 
+  def convert
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys
+
+    @symbol1 = params.fetch("currency1")
+    @symbol2 = params.fetch("currency2")
+    
+    @calculator = open("https://api.exchangerate.host/convert?from=#{@symbol1}&to=#{@symbol2}").read
+    @calculator_data = JSON.parse(@calculator)
+    @exchange_hash = @calculator_data.fetch("info").fetch("rate")
+
+
+    render({:template => "currency_templates/final_step.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   
 get("/forex", {:controller => "currencies", :action => "first_currency"})
 get("/forex/:currency1", {:controller => "currencies", :action => "second_currency"})
+get("/forex/:currency1/:currency2",{:controller => "currencies", :action => "convert"})
 end
